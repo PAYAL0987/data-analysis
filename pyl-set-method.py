368538cdf54a4e659d2e9cec5346f8753093c488
PAYAL0987 <payalkumari9910@gmail.com>
2025-04-16 16:25:16 +0530

    added new file python

---
+++ pyl-set-method.py
@@ -0,0 +1,11 @@
+#s1 = {1,2,3,5}
+#s2 = {3,6,4}
+#print(s1.union(s2))
+#s1.update(s2)
+#print(s1, s2)
+
+
+cities = {"Tokyo", "Madrid", "Berlin","Delhi"}
+cities2 ={"Tokyo", "Seoul", "Kabul", "Madrid"}
+cities3 =  cities.union(cities2)
+print(cities3)
---
+++ pyl-seta.py
@@ -0,0 +1,11 @@
+S = {2, 4, 2,6}
+print(S)
+
+ info = {"Carla", 19, False, 5.9, 19}
+      print(info)
+      
+    payal = set()
+    print(type(payal))
+    
+    for value in info:
+        print(value)
