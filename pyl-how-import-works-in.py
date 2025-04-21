28e3143d0d07fbbc93890173d7ea4eeebfe8bb1b
PAYAL0987 <payalkumari9910@gmail.com>
2025-04-21 15:06:39 +0530

    added new file

--- pyl-how-import-works-in.py
+++ pyl-how-import-works-in.py
@@ -5,6 +5,11 @@
 #math.floor(4.2343) 
 
 #from math import sqrt, pi
-fro6m math import *
-result = sqrt(9) * pi
-print(result)
+#from math import *
+#result = sqrt(9) * pi
+#print(result)
+
+import math 
+
+print(dir(math))
+print(math.nan, type(math,nan))
