285a46f24c40d53648185392019f517d7a6e90b3
PAYAL0987 <payalkumari9910@gmail.com>
2025-04-30 14:07:14 +0530

    added new file

---
+++ pyl-Decorators.py
@@ -0,0 +1,17 @@
+#def greet(fx):
+#def hello():
+ #   print("hello world")
+    
+    def greet(fx):
+        print("GOOD Morning")
+        fx mfa("args" **kwargs)
+        print("Thanks for using this function")
+         return  afx
+     
+    greet
+    hello(a,b):
+    print("Hello world")
+    
+    hello()
+   # greet(add)(1,2)
+   add(1,2)
---
+++ pyl-constructors.py
@@ -0,0 +1,17 @@
+class person :
+
+def __inti__(self,n,o):
+print("Hey I am person")
+self_name = n
+self.occ = o
+
+def info(self):
+    print(f"(self.name) is a (self.occ)")
+    
+ a = person("Payal", "Devloper")
+ b = person("Dviya" "HR")
+ c = person(1,2,3)
+ #print(a.name)
+# a.care = "Divya"
+ #a.occ ="HR"
+ #a.info()
