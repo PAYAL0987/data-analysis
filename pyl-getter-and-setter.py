1dc8233467959ee790215075d8786c20b171b503
PAYAL0987 <payalkumari9910@gmail.com>
2025-05-01 14:01:22 +0530

    added file

---
+++ pyl-getter-and-setter.py
@@ -0,0 +1,18 @@
+class MyClass:
+    def_init_(self, value):
+        self._value
+        def show(self):
+            print(f"Value is (self._value)")
+            
+        cgroperty
+        def tem_value(self._value):
+            return 10 self._value
+        
+        aften_value.setter
+        def tem_value(self, new_value):
+            self._value = new_value/10
+            
+            obj = MyClass(10):
+            obj.tem_value = 12
+            print(obj_tem_Myvalue)
+            obj.show()
---
+++ pyl-inheritance.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, name, id):
+     self.name = name
+     self.id = id 
+     
+     def slowDetalis(self):
+         print(f"The name of[Employee:{self.i} {self.name}]")
+         
+         class Programmer(Employee):
+             def showEmploee(self):
+            print(f"The defult langauge is python"):
+         
+        e1 = Employee("Rohan Das", 400)
+         e1.showDetalis()
+         e2 = [Empolyee("Payal", 1000)]
+         e2.showDetalis()
